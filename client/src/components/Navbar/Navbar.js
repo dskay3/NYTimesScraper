b2ae5d3e422801b67521d459fbe2350f8103aaec
Daniel Kim <dskay7@gmail.com>
2017-10-06 02:18:57 -0400

    Cleaned up Navbar code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -13,35 +13,29 @@ const Navbar = props =>
       </Link>
 
       {/* Mobile: Creates burger icon */}
-      <a href="#" data-activates="mobile-demo" className="button-collapse">
+      <a href="/" data-activates="mobile-demo" className="button-collapse">
         <i className="material-icons">menu</i>
       </a>
 
       {/* Right side Navbar links */}
       <ul id="nav-mobile" className="right hide-on-med-and-down">
+        {/* Link to Main Home Page */}
         <li 
-          className={
-            window.location.pathname === "/" ||
-            window.location.pathname === "/about"
-              ? "active"
-              : ""
-          }
+          className={ window.location.pathname === "/" ? "active" : "" }
         >
           <Link to="/">Home</Link>
         </li>
+
+        {/* Link to Saved Articles */}
         <li 
-          className={
-            window.location.pathname === "/saved" ? "active" : ""
-          }
+          className={ window.location.pathname === "/saved" ? "active" : "" }
         >
           <Link to="/saved">Saved</Link>
         </li>
+
+        {/* Link to Repo */}
         <li 
-          className={
-            window.location.pathname === "/repo" 
-              ? "active" 
-              : ""
-          }
+          className={ window.location.pathname === "/repo" ? "active" : "" }
         >
           <Link to="/repo">Repo</Link>
         </li>
@@ -49,29 +43,23 @@ const Navbar = props =>
 
       {/* Mobile: Navbar links */}
       <ul className="side-nav" id="mobile-demo">
+        {/* Link to Main Home Page */}
         <li 
-          className={
-            window.location.pathname === "/" ||
-            window.location.pathname === "/about"
-              ? "active"
-              : ""
-          }
+          className={ window.location.pathname === "/" ? "active" : "" }
         >
           <Link to="/">Home</Link>
         </li>
+
+        {/* Link to Saved Articles */}
         <li 
-          className={
-            window.location.pathname === "/saved" ? "active" : ""
-          }
+          className={ window.location.pathname === "/saved" ? "active" : "" }
         >
           <Link to="/saved">Saved</Link>
         </li>
+
+        {/* Link to Repo */}
         <li 
-          className={
-            window.location.pathname === "/repo" 
-              ? "active" 
-              : ""
-          }
+          className={ window.location.pathname === "/repo" ? "active" : "" }
         >
           <Link to="/repo">Repo</Link>
         </li>
