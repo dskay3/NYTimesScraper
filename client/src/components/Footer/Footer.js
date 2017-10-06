e3504635bc006bf430666fe7f69bc84f0e48fc72
Daniel Kim <dskay7@gmail.com>
2017-10-06 03:11:23 -0400

    Added Footer styling

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,9 @@ import "./Footer.css";
 // Footer component
 const Footer = () =>
   <footer className="footer">
+    <p className="text-center">
     &copy; Articles are scraped from <a href="https://www.nytimes.com">www.NYTimes.com</a>. Author does not claim copyright for this material.
+    </p>
   </footer>;
 
 export default Footer;
