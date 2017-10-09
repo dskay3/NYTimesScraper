ecdbc15e935cd50200154410785065de539b97d2
Daniel Kim <dskay7@gmail.com>
2017-10-08 23:05:45 -0400

    minor update to styles for mobile

--- client/src/pages/Saved/Saved.js
+++ client/src/pages/Saved/Saved.js
@@ -5,6 +5,8 @@ import Container from "../../components/Container";
 import SavedHeaderContainer from "../../components/SavedHeaderContainer";
 import "./Saved.css";
 import moment from "moment";
+import Row from "../../components/Row";
+import Col from "../../components/Col";
 
 class Saved extends Component {
   // Initial states
@@ -44,37 +46,41 @@ class Saved extends Component {
       <div>
         <SavedHeaderContainer />
 
-        <Container>
-          <ul className="collection with-header saved-results">
-            <li className="collection-header">
-              <h4>List of Saved Articles</h4>
-              <br />
-              <p><strong><ins>Instructions:</ins></strong></p>
-              <p>Click Article to view source.</p>
-              <p>Click <i className="fa fa-trash" aria-hidden="true"></i> to remove from list.</p>
-            </li>
+        <Container style={{ marginTop: 30 }}>
+          <Row>
+            <Col size="sm-12">
+              <ul className="collection with-header">
+                <li className="collection-header">
+                  <h4>List of Saved Articles</h4>
+                  <br />
+                  <p><strong><ins>Instructions:</ins></strong></p>
+                  <p>Click Article to view source.</p>
+                  <p>Click <i className="fa fa-trash" aria-hidden="true"></i> to remove from list.</p>
+                </li>
 
-            { this.state.articles.map(result => 
-              <li key={ result._id } className="collection-item">
-                <div>
-                  <p>
-                    <strong>Article: </strong>
-                    <a className="article-title" href={ result.url }> { result.title }</a>
-                  </p>
+                { this.state.articles.map(result => 
+                  <li key={ result._id } className="collection-item">
+                    <div>
+                      <p>
+                        <strong>Article: </strong>
+                        <a className="article-title" href={ result.url }> { result.title }</a>
+                      </p>
 
-                  <a className="secondary-content right" onClick={ () => this.deleteSavedArticle(result._id) }>
-                    <i className="fa fa-trash save-btn" aria-hidden="true"></i>
-                  </a>
-                </div>
+                      <a className="secondary-content right" onClick={ () => this.deleteSavedArticle(result._id) }>
+                        <i className="fa fa-trash save-btn" aria-hidden="true"></i>
+                      </a>
+                    </div>
 
-                <br />
-                Date Published: { moment(result.date.substring(0, 10)).format('LL') }
-                
-                <br />
-                Date Saved: { moment(result.save_date).add('1', "days").format('LL') }
-              </li>
-            )}
-          </ul>
+                    <br />
+                    Date Published: { moment(result.date.substring(0, 10)).format('LL') }
+                    
+                    <br />
+                    Date Saved: { moment(result.save_date).add('1', "days").format('LL') }
+                  </li>
+                )}
+              </ul>
+            </Col>
+          </Row>
         </Container>
       </div>
     )
