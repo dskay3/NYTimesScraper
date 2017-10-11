abb5cd807e69a251f4790462d7e7003c0bd8e9fb
Daniel Kim <dskay7@gmail.com>
2017-10-11 01:22:51 -0400

    changed image mainHeader

--- client/src/components/MainHeaderContainer/MainHeaderContainer.js
+++ client/src/components/MainHeaderContainer/MainHeaderContainer.js
@@ -4,7 +4,7 @@ import Header from "../../components/Header";
 
 // HeaderContainer component
 const MainHeaderContainer = () =>
-  <Header backgroundImage="https://i.imgur.com/hIxiSKK.jpg">
+  <Header backgroundImage="https://i.imgur.com/Sox1WFN.jpg">
     <h1>The NY Times Article Scraper</h1>
     <p>Articles that are directly scraped from the NY Times</p>
   </Header>;
