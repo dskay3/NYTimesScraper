d0c042326623c0f4a8c802082d19b31cc18cdce5
Daniel Kim <dskay7@gmail.com>
2017-10-11 21:56:34 -0400

    updated scraper text to searcher

--- client/src/components/MainHeaderContainer/MainHeaderContainer.js
+++ client/src/components/MainHeaderContainer/MainHeaderContainer.js
@@ -6,7 +6,7 @@ import Header from "../../components/Header";
 const MainHeaderContainer = () =>
   <Header backgroundImage="https://i.imgur.com/Sox1WFN.jpg">
     <h1>The NY Times Article Searcher</h1>
-    <p>Articles that are directly scraped from the NY Times</p>
+    <p>Articles that are directly searched from the NY Times</p>
   </Header>;
 
 export default MainHeaderContainer;
